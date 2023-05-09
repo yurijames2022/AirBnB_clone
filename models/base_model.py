212b7b074aae67ee72511839ca331312cfd524a4
Frank <wanyoike39@gmail.com>
2023-05-09 07:44:23 +0300

    task 4 complete

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,22 @@ from datetime import datetime
 
 class BaseModel:
     """Class BaseModel"""
-    def __init__(self, id=None, created_at=None, updated_at=None):
+    def __init__(self, *args, **kwargs):
         """object instantiation
         attributes: id
                 created-at
                 updated_at"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.strptime(value, format)
+                if key != '__class__':
+                    self.__dict__[key] = value
 
     def __str__(self):
         """returns string representation of class name, id, and dictionary
