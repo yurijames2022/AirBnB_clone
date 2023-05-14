4648a16db498ffa10153cc3741b2550e2743dcae
James Yuri <yurijamesmail@gmail.com>
2023-05-14 08:49:12 +0300

    filestorage tests update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,7 +23,7 @@ class FileStorage():
 
     def all(self):
         """ returns the dictionary __objects """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
